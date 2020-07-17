2a1f49670c741005e80ad16818100b299945e77d
tatemunnich <tmunnich@macalester.edu>
2020-07-16 21:12:06 -0500

    final changes and deleting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
-// import 'bootstrap/dist/css/bootstrap.css' // messes up my css
 import './css/App.css';
-
 import Results from "./components/results/Results";
 import Header from "./components/layout/Header";
 import SearchBar from "./components/search/SearchBar";
--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import rootReducer from "./reducers";
 import {Provider} from 'react-redux'
 
-const store = createStore(rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+
+const store = createStore(rootReducer, {});
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,8 +17,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
